6f66b9be983a16d102334e7ffd1345b74907d5b0
besesito <bartekszmude@gmail.com>
2021-01-14 15:16:28 +0100

    Add login and polish a little bit

---
+++ login/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.contrib import auth
+
+
+def login(request):
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            username = request.POST['username']
+            auth.login(request, user)
+            return render(request, 'raport/home.html', {'info': 'Siemanko {} mam nadzieję że masz wspaniały dziś dzień! 👍'.format(username)})
+        else:
+            return render(request, 'login/login.html', {'info': "Skleroza? Próbuj dalej! 🤣😂"})
+    else:
+        return render(request, 'login/login.html', {'info':'📢 Nie tak prędko!! Zaloguj się 📢'})
